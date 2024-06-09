d5e354eebc8c278e6f94460cc765205329a8448d
Néfi Carvalho <151886694+Nefi-Carvalho@users.noreply.github.com>
2024-06-09 16:30:04 -0300

    SmartTV

---
+++ tipo-variaveis/src/Operadores.java
@@ -0,0 +1,28 @@
+public class Operadores {
+
+    public static void main(String[] args) {
+
+        boolean variavel = true;
+        
+        int numero = 5;
+
+        int a, b;
+        a = 6;
+        b = 6;
+
+        String resultado = a==b ?"verdadeiro" : "falso";
+
+        System.out.println(resultado);
+        
+        // x repetição
+        //numero++;
+        //numero = numero + 1;
+
+        System.out.println(!variavel);
+
+        System.out.println(numero ++);
+
+        System.out.println(numero);
+    }
+
+}
